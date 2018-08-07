703d2ca6db6bc4629e6f59f1b111ca97cc5fb7c0
Антон Плышевский <plysant@gmail.com>
2018-08-07 20:33:14 +0300

    initial commit

---
+++ solution.py
@@ -0,0 +1,11 @@
+import sys
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+
+d = (b ** 2) - (4 * a * c)
+x1 = (- b + d ** 0.5) / (2 * a)
+x2 = (- b - d ** 0.5) / (2 * a)  
+
+print(int(x1), int(x2), sep='\n')
