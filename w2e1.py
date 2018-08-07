d8ddb40e8fe27354dd55d2da43936988796baf34
Антон Плышевский <plysant@gmail.com>
2018-08-07 22:03:20 +0300

    week 2 exercise 1

---
+++ w2e1.py
@@ -0,0 +1,38 @@
+import os
+import tempfile
+import json
+import argparse
+
+argsparser = argparse.ArgumentParser()
+argsparser.add_argument('--key')
+argsparser.add_argument('--val')
+
+args = argsparser.parse_args()
+
+key = args.key
+value = args.val
+
+data = {}
+
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+
+if os.path.isfile(storage_path):
+    with open(storage_path, 'r') as f:
+        if f.readable():
+            filedata = f.read()
+            if filedata:
+                data = json.loads(filedata)
+
+if value is None:
+    value = data.get(key)
+    if value is None:
+        print(value)
+    else:
+        print(', '.join(data.get(key)))
+else:
+    if key not in data:
+        data[key] = []
+    data[key].append(value)
+    
+    with open(storage_path, 'w') as f:
+        json.dump(data, f)
